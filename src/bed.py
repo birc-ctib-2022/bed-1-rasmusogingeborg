8a595f27bfcf5fb1e56defe6833161b2c6139d1a
Ingeborg3 <111577212+Ingeborg3@users.noreply.github.com>
2022-10-14 11:19:07 +0200

    first

--- src/bed.py
+++ src/bed.py
@@ -15,6 +15,12 @@ BedLine = NamedTuple("BedLine", [
     ('name', str)
 ])
 
+# Type nedenfor skrevet selv:
+BedLine2 = NamedTuple("BedLine2", [
+    ('chrom', str),
+    ('chrom_start', int),
+    ('chrom_end', int)
+])
 
 def parse_line(line: str) -> BedLine:
     """Parse a single line from a BED file (with four columns).
@@ -28,9 +34,21 @@ def parse_line(line: str) -> BedLine:
     """
     chrom, start, end, name = line.split()  # split on any white-space
     bed_line = BedLine(chrom, int(start), int(end), name)
-    assert bed_line.chrom_start + 1 == bed_line.chrom_end
+    assert bed_line.chrom_start + 1 == bed_line.chrom_end 
     return bed_line
 
+# Funktion nedenfor skrevet selv:
+def parse_line2(line: str) -> BedLine2:
+    """
+    Convert a single line from a BED file with three columns into a 
+    BedLine.
+    >>> parse_line2('chr1   20_100  20_101')
+    BedLine2(chrom='chr1', chrom_start=20100, chrom_end=20101)
+    """
+    chrom, start, end = line.split()
+    bed_line = BedLine2(chrom, int(start), int(end))
+    assert bed_line.chrom_start + 1 == bed_line.chrom_end
+    return bed_line
 
 def print_line(line: BedLine, f: TextIO) -> None:
     """Prints line to the stream f as a BED line."""
--- src/format_bed.py
+++ src/format_bed.py
@@ -9,12 +9,49 @@ from bed import (
 )
 
 
+def default(input):
+    """Read lines from input, and prints tab-separated lines to stdout.
+    >>> default(open('data/test_input.bed')) 
+    chr1    20100    20101    foo
+    chr3    0    1    bar
+    """ 
+    for line in input.readlines():
+        elements = line.split()
+        new_line = '    '.join(elements)
+        print(new_line)
+
+def convert_from_files(inp, outp='<stdout>'):
+    """
+    >>> convert_from_files(open('data/test_input.bed', 'r'))
+    chr1    20100    20101    foo
+    chr3    0    1    bar
+    >>> convert_from_files(open('data/test_input.bed', 'r'), open('data/test_output.bed', 'w'))
+    open(data/test_output.bed, 'w') # Virker ikke, men funktionen returnerer jo
+    # også None. 
+    """
+    if outp == '<stdout>':
+        for line in inp.readlines(): # f.readlines is a list of strings.
+            elements = line.split()
+            new_line = '    '.join(elements)
+            print(new_line)
+    else: 
+        for line in inp.readlines():
+            elements = line.split()
+            new_line = '    '.join(elements)
+            print(new_line, file=outp)
+
 def main() -> None:
-    """Run the program."""
-    # Setting up the option parsing using the argparse module
+    """Run the program.    
+    """
+    # Create ArgumentParser-object using the argparse module. The 
+    # ArgumentParser is used to store and parse the options given to 
+    # the program through the command-line. 
     argparser = argparse.ArgumentParser(description="Cleans up a BED file")
     # 'infile' is either provided as an input file name or stdin
-    argparser.add_argument('infile',
+    # add_argument() used to tell the ArgumentParser, which options it can 
+    # expect and which objects it should create when it receives a given 
+    # option.
+    argparser.add_argument('infile',                     # The name of the option passed to the program.
                            nargs='?',                    # 0 or 1 arguments
                            type=argparse.FileType('r'),  # file for reading
                            default=sys.stdin)
@@ -23,12 +60,28 @@ def main() -> None:
                            nargs='?',                    # 0 or 1 arguments
                            type=argparse.FileType('w'),  # file for writing
                            default=sys.stdout)
-
-    # Parse options and put them in the table args
     args = argparser.parse_args()
 
-    # With all the options handled, we just need to do the real work
-    # FIXME: put your code here
+
+    if args.infile.name != '<stdin>' and args.outfile.name != '<stdout>':
+        input = open(args.infile.name, 'r')
+        output = open(args.outfile.name, 'w')
+        for line in input.readlines(): # f.readlines is a list of strings.
+            elements = line.split()
+            new_line = '    '.join(elements)
+            print(new_line, file=output)
+        return
+
+    if args.infile.name != '<stdin>': # and args.outfile.name == <stdout>. 
+        input = open(args.infile.name, 'r')
+        for line in input.readlines():
+            elements = line.split()
+            new_line = '    '.join(elements)
+            print(new_line)
+        return
+
+    default(sys.stdin)
+    return
 
 
 if __name__ == '__main__':
